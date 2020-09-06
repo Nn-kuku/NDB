a8f3fb82b8e515c49f4ca2e250db9d513e4e7088
Nn-kuku <kukoo0129@gmail.com>
2020-09-06 17:44:46 +0900

    add clear command

--- index.js
+++ index.js
@@ -119,7 +119,7 @@ function changeCommandStringLength(str, limitLen = 8) {
 
 client.login(token);
 
-//내꺼
+//감자 명령어
 
 client.on('message', (message) => {
   if (message.content === 'help') {
@@ -142,5 +142,76 @@ client.on('message', (message) => {
   message.channel.send('Ab(에이비)');
   }
 });
+
+client.login(token);
+
+//clear 청소
+
+client.on('message', (message) => {
+  if(message.content.startsWith('청소 ')) {
+      if(checkPermission(message))return;
+  
+      var clearLine = message.content.slice('청소 '.length);
+      var isNum = !isNaN(clearLine)
+  
+      if(isNum && (clearLine <= 0 || 100 < clearLine)) {
+        message.channel.send("1부터 100까지의 숫자만 입력해주세요.")
+        return;
+      } else if(!isNum) {
+        if(message.content.split('<@').length == 2) {
+          if(isNaN(message.content.split(' ')[2])) return;
+  
+          var user = message.content.split(' ')[1].split('<@!')[1].split('>')[0];
+          var count = parseInt(message.content.split(' ')[2])+1;
+          const _limit = 10;
+          let _cnt = 0;
+  
+          message.channel.fetchMessages({limit: _limit}).then(collected => {
+            collected.every(msg => {
+              if(msg.author.id == user) {
+                msg.delete();
+                ++_cnt;
+              }
+              return !(_cnt == count);
+            });
+          });
+        }
+      } else {
+        message.channel.bulkDelete(parseInt(clearLine)+1)
+          .then(() => {
+            AutoMsgDelete(message, `<@${message.author.id}> ` + parseInt(clearLine) + "개의 메시지를 삭제했습니다. (이 메세지는 잠시 후에 사라집니다.)");
+          })
+          .catch(console.error)
+      }
+    }
+})
+
+function checkPermission(message){
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) {
+    message.channel.send(`<@${message.author.id}> ` + "명령어를 수행할 관리자 권한을 소지하고 있지않습니다.")
+    return true;
+  } else {
+    return false;
+  }
+}
+
+async function AutoMsgDelete(message, str, delay = 3000) {
+  let msg = await message.channel.send(str);
+
+  setTimeout(() => {
+    msg.delete();
+  }, delay);
+}
+
+function changeCommandStringLength(str, limitLen = 8) {
+  let tmp = str;
+  limitLen -= tmp.length;
+
+  for(let i=0;i<limitLen;i++) {
+      tmp += ' ';
+  }
+
+  return tmp;
+}
 
 client.login(token);
